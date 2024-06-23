bcf271c85580e05dcaa43bf2b2a1f4f0e58a20de
VINAYAK <vinayakjat.ps11@gmail.com>
2024-06-23 11:35:51 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import NewTask from "./pages/NewTask";
 import Completed from "./pages/Completed";
 import { useState, useEffect } from "react";
